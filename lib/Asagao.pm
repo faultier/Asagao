478bb66ce5ab899633e700a31cb124d35e004fc3
faultier <roteshund+github@gmail.com>
2009-12-05 16:45:15 +0900

    initial commit

---
+++ lib/Asagao.pm
@@ -0,0 +1,33 @@
+package Asagao;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Asagao -
+
+=head1 SYNOPSIS
+
+  use Asagao;
+
+=head1 DESCRIPTION
+
+Asagao is
+
+=head1 AUTHOR
+
+faultier E<lt>faultier@namakemon.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
